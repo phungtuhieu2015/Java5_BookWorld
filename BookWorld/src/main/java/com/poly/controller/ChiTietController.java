964839c153e404396cf18277e6c53f3d5d826dd4
HuuTaiOne <nguyentaint979@gmail.com>
2023-05-23 14:00:24 +0700

    Hữu Tài - Đã sửa thêm các giao diện và controller lien-he,gio-hang,chi-tiet

--- BookWorld/src/main/java/com/poly/controller/ChiTietController.java
+++ BookWorld/src/main/java/com/poly/controller/ChiTietController.java
@@ -2,8 +2,10 @@ package com.poly.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/bookworld")
 public class ChiTietController {
     
     @GetMapping("/chitiet")
