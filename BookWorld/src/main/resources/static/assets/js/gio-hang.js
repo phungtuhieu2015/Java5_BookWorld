baff5218a015c7aa7c8f1b7ded5f17dc35edbf46
phungtuhieu2015 <phungtuhieu2015@gmail.com>
2023-05-26 23:53:17 +0700

    TuHieu - Sửa giỏ hàng 50%

--- BookWorld/src/main/resources/static/assets/js/gio-hang.js
+++ BookWorld/src/main/resources/static/assets/js/gio-hang.js
@@ -14,27 +14,28 @@ $.ajax({
     }
   });
 }
-function removeCart(item,iStat){
-    var choice = confirm("Bạn có chắc muốn xóa?");
-    if(choice == false) {
-        return;
-    }
+// function removeCart(item,iStat){
+//     var choice = confirm("Bạn có chắc muốn xóa?");
+//     if(choice == false) {
+//         return;
+//     }
     
-    $.ajax({
-        type: "POST",
-        url: "/cart/item/remove",
-        data: JSON.stringify(item),
-        contentType: "application/json",
-        dataType: "text",
-        success: function(resp) {
-            alert(resp)
-            $(".basket-product")[iStat.index].remove();
-        },
-        error: function(error) {
-            console.log(error);
-        }
-    });
-}
+//     $.ajax({
+//         type: "POST",
+//         url: "/cart/item/remove",
+//         data: JSON.stringify(item),
+//         contentType: "application/json",
+//         dataType: "text",
+//         success: function(resp) {
+//             alert(resp)
+//             $(".basket-product").eq(iStat.index).remove();
+//         },
+//         error: function(error) {
+//             console.log(error);
+//         }
+//     });
+// }
+
 function removeCartAll(){
     var choice = confirm("Bạn có chắc muốn xóa tất cả?");
     if(choice == false) {
