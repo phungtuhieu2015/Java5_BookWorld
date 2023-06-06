a2b4de5112d26935efafdc807ba89ea4cb1a77fd
Nhdinh <nhdinhpc03@gmail.com>
2023-06-06 08:06:44 +0700

    Hoàng Dinh xong 70% đang sửa lỗi hình , chức năng thêm sửa

--- BookWorld/src/main/java/com/poly/controller/admin/UsersController.java
+++ BookWorld/src/main/java/com/poly/controller/admin/UsersController.java
@@ -49,7 +49,6 @@ public class UsersController {
  public String edit(Model model, @PathVariable("username") String username) {
   model.addAttribute("pageName", "users user");
      model.addAttribute("form", true);
-
      model.addAttribute("isEdit", true);
      User item = dao.findById(username).get();
      model.addAttribute("item", item);
@@ -58,25 +57,26 @@ public class UsersController {
      return "admin/index-admin";
  }
 
-  // @RequestMapping("/users/create")
-  // public String create(User item) {
-  //     dao.save(item);
-  //     form = false;
-  //     return "admin/index-admin";
-  // }
+   @RequestMapping("/users/create")
+   public String create(User item) {
+       dao.save(item);
+       form = false;
+       return "admin/index-admin";
+   }
 
-  // @RequestMapping("/users/update")
-  //    public String update(User item,Model model) {
-  //        dao.save(item);
-  //        return "redirect:/admin/users/edit/" + item.getUsername();
-  //    }
+   @RequestMapping("/users/update")
+    public String update(User item) {
+        dao.save(item);
+           
+        return "redirect:/admin/users/edit/" + item.getUsername();
+    }
 
 
-    //  @RequestMapping("/users/delete/{username}")
-    //  public String delete(@PathVariable("username") String username) {
-    //      dao.deleteById(username);
-    //      return "redirect:/admin/users";
-    //  }
+      @RequestMapping("/users/delete/{username}")
+      public String delete(@PathVariable("username") String username) {
+          dao.deleteById(username);
+          return "redirect:/admin/users";
+      }
 
     @RequestMapping("/users/reset")
     public String reset(Model model) {
