1b4fcc3d94acce4f7dbc7a52c459cc4f0fa818fc
phungtuhieu2015 <phungtuhieu2015@gmail.com>
2023-06-16 12:34:55 +0700

    Tự Hiếu - thêm tìm kiếm sách

--- BookWorld/src/main/java/com/poly/dao/BookDAO.java
+++ BookWorld/src/main/java/com/poly/dao/BookDAO.java
@@ -6,7 +6,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import com.poly.model.Author;
 import com.poly.model.Book;
 import com.poly.model.Category;
 import com.poly.model.ReportSoldBooks;
@@ -26,6 +28,8 @@ public interface BookDAO extends JpaRepository<Book, String> {
     Page getSoldBooks(Pageable pageable);
     Page findByStatusNot(Integer status,Pageable pageable);
 
+     @Query("SELECT b FROM Book b WHERE b.id LIKE  CONCAT('%',:keywords, '%') OR b.title LIKE CONCAT('%',:keywords, '%')")
+    Page findByIdOrTitle(@Param("keywords") String keywords, Pageable pageable);
     // Page findByCategory(Category category ,Pageable pageable);
 
     
